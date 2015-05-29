c79ffff38c908d94d63c08b3d4660c19bcaf7c38
Macpon7 <adrianceng@gmail.com>
2015-05-29 12:37:41 +0200

    Finished countdown for

--- Countdown(For).cpp
+++ Countdown(For).cpp
@@ -1,3 +1,17 @@
 //Countdown using a for loop
 #include <iostream>
 #include <string>
+ using namespace std;
+ 
+ int main() {
+	 //For loop, n is 10, while n is greater than zero -> decrease n by one
+	 for (int n=10; n>0; --n) {
+		 //Every time the loop loops, print n
+		 cout << n <<", ";
+	 }
+	 //As n is finally zero, print "Liftoff!"
+	 cout << "liftoff!";
+	 
+	 //Terminate
+	 return 0;
+ }
